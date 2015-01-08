2490e616c685d50d21b1150c886ffdb30b0d727b
t.hoefler <t.hoefler@141.19.144.34>
2015-01-08 12:14:53 +0100

    paar neue klassen

---
+++ src/Wärmetauscher.java
@@ -0,0 +1,8 @@
+
+public class Wärmetauscher {
+	
+
+    public static int wärmeAusgleichen(int warmwasserTemperatur, int kaltwasserTemperatur){
+        return (int)((warmwasserTemperatur + kaltwasserTemperatur)/2);
+}
+}
