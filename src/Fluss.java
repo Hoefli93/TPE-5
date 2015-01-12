bf117140515ccc3fc3493b1089b704ca45ce7838
t.hoefler <t.hoefler@141.19.144.79>
2015-01-12 15:12:36 +0100

    bissl weiter

--- src/Fluss.java
+++ src/Fluss.java
@@ -1,4 +1,29 @@
 
-public class Fluss {
 
-}
+	public class Fluss extends Temperatur {
+
+		private Leitware leitware;
+
+
+		public Fluss(int temperatur,Leitware leitware) {
+			super(temperatur);
+			this.leitware = leitware;
+			
+		}
+		
+		
+		
+		@Override
+	    public int getTemperatur(){
+	        return 10;
+	    }
+	    
+		
+		// wenn die TEmperatur ge�ndert wird wird diese an die Leitwarte weitergeleitet;
+		@Override
+	    public void setTemperature(int temp){
+	    
+	    }
+
+	}
+
--- src/Temperatur.java
+++ src/Temperatur.java
@@ -1,4 +1,18 @@
 
-public class Temperatur {
+public abstract class Temperatur {
+
+	private volatile int temperatur;
+
+	public Temperatur(int temperatur) {
+		this.temperatur = temperatur;
+	}
+
+	public int getTemperatur() {
+		return temperatur;
+	}
+
+	public void setTemperature(int temperatur) {
+		this.temperatur = temperatur;
+	}
 
 }
